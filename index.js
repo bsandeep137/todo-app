ca7c68f52e8c534f1d864cbd2cf11c40b0a045b9
bachus <bachus@opentext.com>
2024-07-14 13:17:15 +0530

    pushing backend changes

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const app = express();
+const {createTodo, updateTodo} = require("./types");
 
 app.use(express.json());
 
 
 
-app.get('/todos', function(){});
+app.get('/todos', function(){
+    const createPayload = req.body;
+    const parsedBody = createTodo.safeParse(createPayload);
+    if(!parsedBody.success){
+        res.status(411).json({
+            msg: "ypu sent the wrong inputs"
+        })
+        return;
+    }
+});
 
 
 app.post("/todo", function(req,res){});
 
 
-app.put("/completed", function(req,res){});
+app.put("/completed", function(req,res){
+    const updatePayload = req.body;
+    const parsedBody = updateTodo.safeParse(updatePayload);
+    if(!parsedBody.success){
+        res.status(411).json({
+            msg: "Wrong data"
+        })
+        return;
+    }
+});
